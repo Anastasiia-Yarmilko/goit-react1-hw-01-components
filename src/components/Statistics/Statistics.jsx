646334d300c38daececce2b402e755d152f46afd
Anastasiia Yarmilko <anastasiia.yarm@gmail.com>
2021-09-23 00:25:23 +0300

    Makes task 2

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,19 +1,20 @@
+/* eslint-disable array-callback-return */
 import React from "react";
 import PropTypes from "prop-types";
 import css from "./Statistics.module.css"
 
 export const Statistics = ({ title, stats, children }) => {
     return (
-        <section>
+        <section className={css.statistics}>
             { title && <h2 className={css.title}>{title}</h2>}
             {children}
             <ul className={css.stat_list}>
-                {stats.map(({ id, label, percentage }) => {
+                {stats.map(({ id, label, percentage }) => (
                     <li className={css.item} key={id} >
                         <span className={css.label}>{label}</span>
                         <span className={css.percentage}>{percentage} %</span>
                    </li>
-                })}
+                ))}
             </ul>
         </section>
     )
@@ -21,7 +22,7 @@ export const Statistics = ({ title, stats, children }) => {
     
 Statistics.propTypes = {
     title: PropTypes.string.isRequired,
-    id: PropTypes.string.isRequired,
-    label: PropTypes.string.isRequired,
-    percentage: PropTypes.number.isRequired,
+    id: PropTypes.string,
+    label: PropTypes.string,
+    percentage: PropTypes.number,
 }
